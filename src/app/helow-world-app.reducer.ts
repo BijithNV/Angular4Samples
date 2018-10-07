fdc4254accda3cb69e7379c5093644c2c716e713
BijithNV <nvbijith@gmail.com>
2018-10-06 23:47:59 -0400

    merged both changes

--- src/app/helow-world-app.reducer.ts
+++ src/app/helow-world-app.reducer.ts
@@ -3,4 +3,8 @@ import{ Action } from '@ngrx/store';
 // export interface AppState{
 //     products:Array<product
 // }
+<<<<<<< Updated upstream
 // Test Test updated
+=======
+// Test Test again test
+>>>>>>> Stashed changes
